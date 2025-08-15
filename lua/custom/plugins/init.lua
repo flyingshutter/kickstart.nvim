b1845db5dfb391d20607d70681d2feae64b20936
flyingshutter <alois1.schneider@googlemail.com>
2025-08-16 00:12:56 +0200

    feature: move my plugins to new location and make keymap they now live in lua/custom/plugins/init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,72 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'tpope/vim-fugitive',
+    'mfussenegger/nvim-dap',
+    'nvim-neotest/nvim-nio',
+    { 'rcarriga/nvim-dap-ui', opts = {} },
+    {
+      'mfussenegger/nvim-dap-python',
+      config = function()
+        require('dap-python').setup 'python3'
+        local dap, dapui = require 'dap', require 'dapui'
+        dap.listeners.before.attach.dapui_config = function()
+          dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+          dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+          dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+          dapui.close()
+        end
+      end,
+    },
+
+    vim.keymap.set('n', '<F5>', function()
+      require('dap').continue()
+    end),
+    vim.keymap.set('n', '<F10>', function()
+      require('dap').step_over()
+    end),
+    vim.keymap.set('n', '<F11>', function()
+      require('dap').step_into()
+    end),
+    vim.keymap.set('n', '<F12>', function()
+      require('dap').step_out()
+    end),
+    vim.keymap.set('n', '<Leader>b', function()
+      require('dap').toggle_breakpoint()
+    end),
+    vim.keymap.set('n', '<Leader>B', function()
+      require('dap').set_breakpoint()
+    end),
+    vim.keymap.set('n', '<Leader>lp', function()
+      require('dap').set_breakpoint(nil, nil, vim.fn.input 'Log point message: ')
+    end),
+    vim.keymap.set('n', '<Leader>dr', function()
+      require('dap').repl.open()
+    end),
+    vim.keymap.set('n', '<Leader>dl', function()
+      require('dap').run_last()
+    end),
+    vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
+      require('dap.ui.widgets').hover()
+    end),
+    vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
+      require('dap.ui.widgets').preview()
+    end),
+    vim.keymap.set('n', '<Leader>df', function()
+      local widgets = require 'dap.ui.widgets'
+      widgets.centered_float(widgets.frames)
+    end),
+    vim.keymap.set('n', '<Leader>ds', function()
+      local widgets = require 'dap.ui.widgets'
+      widgets.centered_float(widgets.scopes)
+    end),
+  },
+}
