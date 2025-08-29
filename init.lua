7b7c76393c06696c77ca1e7e2f3b78cdd980b94c
flyingshutter <alois1.schneider@googlemail.com>
2025-08-29 12:17:05 +0200

    initial commit

--- init.lua
+++ init.lua
@@ -166,6 +166,11 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+-- AS add
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
