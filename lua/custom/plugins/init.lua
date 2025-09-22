94c302e8e4f66860ce3bd04eadd3507c748db65a
flyingshutter <alois1.schneider@googlemail.com>
2025-09-23 01:48:41 +0200

    feature: added theHamsta/nvim-dap-virtual-text

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,11 +7,13 @@ return {
     'tpope/vim-fugitive',
     'mfussenegger/nvim-dap',
     'nvim-neotest/nvim-nio',
+    'theHamsta/nvim-dap-virtual-text',
     { 'rcarriga/nvim-dap-ui', opts = {} },
     {
       'mfussenegger/nvim-dap-python',
       config = function()
         require('dap-python').setup 'python' -- AS: windows HAS to differ in this line
+        require('nvim-dap-virtual-text').setup {}
         local dap, dapui = require 'dap', require 'dapui'
         dap.listeners.before.attach.dapui_config = function()
           dapui.open()
