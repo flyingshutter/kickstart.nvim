953fb51832c144551ad3b4296494b9878d0587de
flyingshutter <alois1.schneider@googlemail.com>
2025-09-23 02:34:09 +0200

    refactor: clean up dap config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,13 +7,16 @@ return {
     'tpope/vim-fugitive',
     'mfussenegger/nvim-dap',
     'nvim-neotest/nvim-nio',
-    'theHamsta/nvim-dap-virtual-text',
-    { 'rcarriga/nvim-dap-ui', opts = {} },
     {
-      'mfussenegger/nvim-dap-python',
+      'theHamsta/nvim-dap-virtual-text',
       config = function()
-        require('dap-python').setup 'python' -- AS: windows HAS to differ in this line
         require('nvim-dap-virtual-text').setup {}
+      end,
+    },
+    {
+      'rcarriga/nvim-dap-ui',
+      config = function()
+        require('dapui').setup()
         local dap, dapui = require 'dap', require 'dapui'
         dap.listeners.before.attach.dapui_config = function()
           dapui.open()
@@ -27,6 +30,12 @@ return {
         dap.listeners.before.event_exited.dapui_config = function()
           dapui.close()
         end
+      end,
+    },
+    {
+      'mfussenegger/nvim-dap-python',
+      config = function()
+        require('dap-python').setup 'python' -- AS: windows HAS to differ in this line
       end,
     },
 
