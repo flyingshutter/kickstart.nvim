2e3919cb6cdc2b1af5f1ca2a58c58bd00609ba6a
flyingshutter <alois1.schneider@googlemail.com>
2025-09-23 02:50:26 +0200

    refactor: cleanup dap config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,64 +3,67 @@
 --
 -- See the kickstart.nvim README for more information
 return {
+  'tpope/vim-fugitive',
+  'mfussenegger/nvim-dap',
+  'nvim-neotest/nvim-nio',
   {
-    'tpope/vim-fugitive',
-    'mfussenegger/nvim-dap',
-    'nvim-neotest/nvim-nio',
-    {
-      'theHamsta/nvim-dap-virtual-text',
-      config = function()
-        require('nvim-dap-virtual-text').setup {}
-      end,
-    },
-    {
-      'rcarriga/nvim-dap-ui',
-      config = function()
-        require('dapui').setup()
-        local dap, dapui = require 'dap', require 'dapui'
-        dap.listeners.before.attach.dapui_config = function()
-          dapui.open()
-        end
-        dap.listeners.before.launch.dapui_config = function()
-          dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-          dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-          dapui.close()
-        end
-      end,
-    },
-    {
-      'mfussenegger/nvim-dap-python',
-      config = function()
-        require('dap-python').setup 'python' -- AS: windows HAS to differ in this line
-      end,
-    },
+    'theHamsta/nvim-dap-virtual-text',
+    config = function()
+      require('nvim-dap-virtual-text').setup {}
+    end,
+  },
+  {
+    'mfussenegger/nvim-dap-python',
+    config = function()
+      require('dap-python').setup 'python' -- AS: windows HAS to differ in this line
+    end,
+  },
+  {
+    'rcarriga/nvim-dap-ui',
+    config = function()
+      require('dapui').setup()
 
-    vim.keymap.set('n', '<F5>', require('dap').continue, { desc = 'dap continue' }),
-    vim.keymap.set('n', '<F6>', require('dap').step_over, { desc = 'dap step_over' }),
-    vim.keymap.set('n', '<F7>', require('dap').step_into, { desc = 'dap step into' }),
-    vim.keymap.set('n', '<F8>', require('dap').step_out, { desc = 'dap step out' }),
-    vim.keymap.set('n', '<F9>', require('dap').terminate, { desc = 'dap terminate debug session' }),
-    vim.keymap.set('n', '<Leader>b', require('dap').toggle_breakpoint, { desc = 'Toggle [b]reakpoint' }),
-    vim.keymap.set('n', '<Leader>B', require('dap').set_breakpoint, { desc = 'Set [B]reakpoint' }),
-    vim.keymap.set('n', '<Leader>lp', function()
-      require('dap').set_breakpoint(nil, nil, vim.fn.input 'Log point message: ')
-    end, { desc = 'Set [L]og [P]oint with message' }),
-    vim.keymap.set('n', '<Leader>dr', require('dap').repl.open, { desc = '[D]ebug Open [r]epl' }),
-    vim.keymap.set('n', '<Leader>dl', require('dap').run_last, { desc = '[D]ebug Run [L]ast' }),
-    vim.keymap.set({ 'n', 'v' }, '<Leader>dh', require('dap.ui.widgets').hover, { desc = '[D]ebug [H]over' }),
-    vim.keymap.set({ 'n', 'v' }, '<Leader>dp', require('dap.ui.widgets').preview, { desc = '[D]ebug [P]review' }),
-    vim.keymap.set('n', '<Leader>df', function()
-      local widgets = require 'dap.ui.widgets'
-      widgets.centered_float(widgets.frames)
-    end, { desc = '[D]ebug [F]rames' }),
-    vim.keymap.set('n', '<Leader>ds', function()
-      local widgets = require 'dap.ui.widgets'
-      widgets.centered_float(widgets.scopes)
-    end, { desc = '[D]ebug [S]copes' }),
+      -- automatically open and close dapui
+      local dap, dapui = require 'dap', require 'dapui'
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+
+      -- dap keymaps
+      vim.keymap.set('n', '<F5>', require('dap').continue, { desc = 'dap continue' })
+      vim.keymap.set('n', '<F6>', require('dap').step_over, { desc = 'dap step_over' })
+      vim.keymap.set('n', '<F7>', require('dap').step_into, { desc = 'dap step into' })
+      vim.keymap.set('n', '<F8>', require('dap').step_out, { desc = 'dap step out' })
+      vim.keymap.set('n', '<F9>', require('dap').terminate, { desc = 'dap terminate debug session' })
+      vim.keymap.set('n', '<Leader>b', require('dap').toggle_breakpoint, { desc = 'Toggle [b]reakpoint' })
+      vim.keymap.set('n', '<Leader>B', require('dap').set_breakpoint, { desc = 'Set [B]reakpoint' })
+      vim.keymap.set('n', '<Leader>lp', function()
+        require('dap').set_breakpoint(nil, nil, vim.fn.input 'Log point message: ')
+      end, { desc = 'Set [L]og [P]oint with message' })
+      vim.keymap.set('n', '<Leader>dr', require('dap').repl.open, { desc = '[D]ebug Open [r]epl' })
+      vim.keymap.set('n', '<Leader>dl', require('dap').run_last, { desc = '[D]ebug Run [L]ast' })
+
+      -- dapui keymaps
+      vim.keymap.set({ 'n', 'v' }, '<Leader>dh', require('dap.ui.widgets').hover, { desc = '[D]ebug [H]over' })
+      vim.keymap.set({ 'n', 'v' }, '<Leader>dp', require('dap.ui.widgets').preview, { desc = '[D]ebug [P]review' })
+      vim.keymap.set('n', '<Leader>df', function()
+        local widgets = require 'dap.ui.widgets'
+        widgets.centered_float(widgets.frames)
+      end, { desc = '[D]ebug [F]rames' })
+      vim.keymap.set('n', '<Leader>ds', function()
+        local widgets = require 'dap.ui.widgets'
+        widgets.centered_float(widgets.scopes)
+      end, { desc = '[D]ebug [S]copes' })
+    end,
   },
   {
     'ThePrimeagen/harpoon',
