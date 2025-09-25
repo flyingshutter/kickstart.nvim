51205689c1f33f131af115b056f9e3bcc1759d88
flyingshutter <alois1.schneider@googlemail.com>
2025-09-25 16:48:07 +0200

    workaround: pyright has a bug with nvim0.12
    see https://github.com/neovim/neovim/issues/34731

--- lua/custom/alois.lua
+++ lua/custom/alois.lua
@@ -27,3 +27,74 @@ vim.keymap.set('n', '<leader>ma', function()
     end
   end)
 end, { desc = 'Run a Vim command using vim.ui.input' })
+
+local ls = require 'luasnip'
+local s = ls.snippet
+local i = ls.insert_node
+local t = ls.text_node
+local fmt = require('luasnip.extras.fmt').fmt
+
+ls.add_snippets('all', {
+  s(
+    'ifname',
+    fmt(
+      [[
+    if __name__ == "__main__":
+        main({})
+    ]],
+      {
+        i(1, 'params'),
+      }
+    )
+  ),
+  s(
+    'example2',
+    fmt(
+      [[
+      if {} then
+        {}
+      end
+      ]],
+      {
+        -- i(1) is at nodes[1], i(2) at nodes[2].
+        i(1, 'not now'),
+        i(2, 'when'),
+      }
+    )
+  ),
+})
+
+-- AS as of end of September 2025, pyright causes a bug from neovim version >=0.12,
+-- see https://github.com/neovim/neovim/issues/34731
+-- this is a workaround
+vim.lsp.config('pyright', {
+  handlers = {
+    -- Override the default rename handler to remove the `annotationId` from edits.
+    --
+    -- Pyright is being non-compliant here by returning `annotationId` in the edits, but not
+    -- populating the `changeAnnotations` field in the `WorkspaceEdit`. This causes Neovim to
+    -- throw an error when applying the workspace edit.
+    --
+    -- See:
+    -- - https://github.com/neovim/neovim/issues/34731
+    -- - https://github.com/microsoft/pyright/issues/10671
+    [vim.lsp.protocol.Methods.textDocument_rename] = function(err, result, ctx)
+      if err then
+        vim.notify('Pyright rename failed: ' .. err.message, vim.log.levels.ERROR)
+        return
+      end
+
+      ---@cast result lsp.WorkspaceEdit
+      for _, change in ipairs(result.documentChanges or {}) do
+        for _, edit in ipairs(change.edits or {}) do
+          if edit.annotationId then
+            edit.annotationId = nil
+          end
+        end
+      end
+
+      local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
+      vim.lsp.util.apply_workspace_edit(result, client.offset_encoding)
+    end,
+  },
+})
