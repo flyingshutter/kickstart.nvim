00cb45c0d0fe7307f91bc8c38ae341397b2e45d2
flyingshutter <alois1.schneider@googlemail.com>
2025-10-01 02:43:30 +0200

    feat[Telescope]: <leader>sp search plugin folder

--- init.lua
+++ init.lua
@@ -553,6 +553,9 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+      vim.keymap.set('n', '<leader>sp', function()
+        builtin.find_files { cwd = '~/.local/share/nvim/lazy' }
+      end, { desc = '[S]earch [P]lugin files' })
     end,
   },
 
