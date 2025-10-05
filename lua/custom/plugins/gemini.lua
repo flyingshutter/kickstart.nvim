9fbf8cf541df2c6a6bb6453dbbce86275ea79bb1
flyingshutter <alois1.schneider@googlemail.com>
2025-10-05 17:53:07 +0200

    update[gemini]: update config to new name gemini-autocomplete

--- lua/custom/plugins/gemini.lua
+++ lua/custom/plugins/gemini.lua
@@ -42,16 +42,26 @@ return {
     },
   },
   {
-    dir = '~/as/Projekte/2025-09-28_nvim_plugins/gemini.nvim/',
+    dir = '~/as/Projekte/2025-09-28_nvim_plugins/gemini-autocomplete.nvim/',
     config = function()
-      local gemini = require 'gemini'
+      local gemini = require 'gemini-autocomplete'
       gemini.setup {
-        general = { mini_statusline = true },
+        general = { make_statusline = require('gemini-autocomplete.external').make_mini_statusline },
+        model = {
+          model_id = require('gemini-autocomplete.api').MODELS.GEMINI_2_5_FLASH_LITE,
+        },
+        -- I like to have it disabled on startup and manually activate when needed (free tier user, quota matters)
         completion = { enabled = false },
       }
 
-      vim.keymap.set('n', '<leader>ac', gemini.toggle_enabled, { desc = 'Toggle Gemini [a]uto[c]ompletion' })
-      vim.keymap.set('n', '<leader>af', gemini.add_gitfiles, { desc = 'Gemini add Git[f]iles' })
+      require('gemini-autocomplete.external').make_mini_statusline() -- show gemini in statusline and indicate (en/dis)abled
+
+      vim.keymap.set('n', '<leader>gt', gemini.toggle_enabled, { desc = '[G]emini [T]oggle Autocompletion' })
+      vim.keymap.set('n', '<leader>gg', gemini.add_gitfiles, { desc = '[G]emini add [G]itfiles' })
+      vim.keymap.set('n', '<leader>ge', gemini.edit_context, { desc = '[G]emini [E]dit Context' })
+      vim.keymap.set('n', '<leader>gp', gemini.prompt_code, { desc = '[G]emini [P]rompt Code' })
+      vim.keymap.set('n', '<leader>gc', gemini.clear_context, { desc = '[G]emini [C]lear Context' })
+      vim.keymap.set('n', '<leader>gm', gemini.choose_model, { desc = '[G]emini Choose [M]odel' })
     end,
   },
 }
