b45ad76e2e6c2519d8068e0951881b7f4c50aae6
flyingshutter <alois1.schneider@googlemail.com>
2026-01-29 15:05:34 +0100

    chore [init]: mv alois.lua to scripts/

--- init.lua
+++ init.lua
@@ -1139,7 +1139,7 @@ require('lazy').setup({
   },
 })
 
-require 'custom.alois'
+require 'custom.scripts.alois'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/custom/scripts/alois.lua
+++ lua/custom/scripts/alois.lua
@@ -213,3 +213,50 @@ vim.keymap.set('n', '<leader>dm', function()
   print(result)
   require('dap').continue()
 end, { desc = '[D]ebug: [M]ake and Start' })
+
+
+---------------- Settings for working with ESP8266 -------------------
+----------------------------------------------------------------------
+vim.lsp.config('lua_ls', {
+  settings = {
+    Lua = {
+      -- runtime = {
+      --   version = 'Lua 5.1', -- Essential for NodeMCU compatibility
+      -- },
+      diagnostics = {
+        -- Prevents red squiggles for NodeMCU-specific globals
+        globals = { 'vim', 'node', 'wifi', 'gpio', 'net', 'tmr', 'uart', 'file' },
+      },
+      workspace = {
+        library = {
+          -- Load Neovim's built-in Lua API types
+          vim.api.nvim_get_runtime_file("", true),
+          -- Path to your NodeMCU API definitions
+          "/home/sca04245/as/rechner/importantfiles/nodeMCU-emmylua/esp8266/library/",
+        },
+        -- checkThirdParty = false,
+      },
+      -- telemetry = { enable = false },
+    },
+  },
+})
+-- After defining the config, you usually "enable" it for the buffers
+-- vim.lsp.enable('lua_ls')
+
+-- Upload current file to NodeMCU and open terminal
+vim.keymap.set('n', '<leader>eu', function()
+  local res = vim.fn.system("nodemcu-tool devices --json 2>/dev/null")
+  res = vim.json.decode(res)
+  local esp_path = res[1].path
+
+  local file = vim.fn.expand('%:p')
+  vim.cmd("vsplit | term bash -ci 'nodemcu-tool --port " .. esp_path .. " upload " .. file .. " && nodemcu-tool terminal'")
+end, { desc = "Upload Esp8266 and Terminal" })
+
+vim.keymap.set('n', '<leader>er', function()
+  local res = vim.fn.system("nodemcu-tool devices --json 2>/dev/null")
+  res = vim.json.decode(res)
+  local esp_path = res[1].path
+
+  vim.cmd("vsplit | term bash -ci 'nodemcu-tool --port " .. esp_path .. " reset && nodemcu-tool terminal'")
+end, { desc = "Reset Esp8266 and Terminal" })
